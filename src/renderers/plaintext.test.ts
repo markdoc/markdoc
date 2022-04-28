64c042c0d5f604f6b2a0d607cbdb561c7320a01a
Mike Fix <mfix@stripe.com>
2022-04-28 16:44:58 -0700

    expected

--- src/renderers/plaintext.test.ts
+++ src/renderers/plaintext.test.ts
@@ -42,10 +42,56 @@ Markdoc uses a fully declarative approach to composition and flow control, where
 | Paragraph        | Text        |
 `;
 
+const expected = `---
+title: What is Markdoc?
+---
+
+# {% $markdoc.frontmatter.title %} 
+
+Markdoc is a **Markdown**-based \`syntax\` and _toolchain_ for creating custom documentation sites. Stripe created Markdoc to power [http://stripe.com/docs](our public docs).
+
+> Blockquote
+
+---
+
+![Alt](/image)
+
+{% callout a="check" b={"e":5} c=8 d=[1,2,3] %}
+
+Markdoc is open-source—check out it's [http://github.com/markdoc/markdoc](source) to see how it works.
+
+{% /callout %}
+
+\`\`\`js
+Code!
+\`\`\`
+
+## How is {% markdoc("test", 1) %} different? 
+
+foo\\
+baz
+
+Soft
+break
+Markdoc uses a fully declarative approach to composition and flow control, where other solutions…[/docs/overview](read more)
+
+## Next steps
+
+- [/docs/getting-started](Install Markdoc)
+- [/sandbox](Try it out online)
+
+| Syntax      | Description |
+| ----------- | ----------- |
+| Header      | Title       |
+| Paragraph   | Text        |
+`;
+
 const ast = Markdoc.parse(source);
 
 fdescribe('Plaintext renderer', function () {
   it('rendering a tag', function () {
-    console.log(render(ast));
+    const doc = render(ast);
+    console.log(doc);
+    expect(doc).toEqual(expected);
   });
 });
