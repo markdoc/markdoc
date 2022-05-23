2b9334f6186dbbaf1e4b2e5da1369ca8b18717f8
Mike Fix <mfix@stripe.com>
2022-05-23 14:23:32 -0700

    fix delimiters bug

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,4 @@
+import { Delimiter } from 'markdown-it/lib/rules_inline/state_inline';
 import type { ValidationError } from './src/types';
 
 declare module 'markdown-it/lib/token' {
@@ -7,5 +8,11 @@ declare module 'markdown-it/lib/token' {
       start?: number;
       end?: number;
     };
+  }
+}
+
+declare module 'markdown-it/lib/rules_block/state_block' {
+  export default class Token extends require('markdown-it/lib/rules_block/state_block') {
+    delimiters?: Delimiter[];
   }
 }
