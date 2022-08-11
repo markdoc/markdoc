7611e4b63be7c3db184b000be8ce1efdd58c00b9
Mike Fix <mfix@stripe.com>
2022-08-11 14:01:08 -0700

    expose in renderers

--- src/renderers/index.ts
+++ src/renderers/index.ts
@@ -1,5 +1,6 @@
 import html from './html';
 import react from './react/react';
 import reactStatic from './react/static';
+import UNSTABLE_DO_NOT_USE_pretty_render from './unstable_pretty';
 
-export default { html, react, reactStatic };
+export default { html, react, reactStatic, UNSTABLE_DO_NOT_USE_pretty_render };
