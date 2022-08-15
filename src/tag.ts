0591f93c753e3ec10d4afe56f4cacf3cdcfbfd3e
Mike Fix <mfix@stripe.com>
2022-08-15 13:38:57 -0700

    make Tag a generic class
    Closes #169

--- src/tag.ts
+++ src/tag.ts
@@ -1,15 +1,18 @@
 import type { RenderableTreeNode } from './types';
 
-export default class Tag {
+export default class Tag<
+  N extends string = string,
+  A extends Record<string, any> = Record<string, any>
+> {
   readonly $$mdtype = 'Tag' as const;
 
-  name: string;
-  attributes: Record<string, any>;
+  name: N;
+  attributes: A;
   children: RenderableTreeNode[];
 
   constructor(
-    name = 'div',
-    attributes = {},
+    name = 'div' as N,
+    attributes = {} as A,
     children: RenderableTreeNode[] = []
   ) {
     this.name = name;
