c592164e005ab191fda2ec46a5108ccbb4c2dd78
Mike Fix <mfix@stripe.com>
2022-10-21 14:28:44 -0700

    Forward React.forwardRef react nodes
    Closes https://github.com/markdoc/markdoc/discussions/255

--- src/renderers/react/shared.ts
+++ src/renderers/react/shared.ts
@@ -7,7 +7,7 @@ export function tagName(
   components: Record<string, Component> | ((string: string) => Component)
 ): string | Component {
   return typeof name !== 'string'
-    ? 'Fragment'
+    ? name // This can be an object, e.g. when React.forwardRef is used
     : name[0] !== name[0].toUpperCase()
     ? name
     : components instanceof Function
