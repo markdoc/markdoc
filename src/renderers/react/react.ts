ea15b9714d004e116509c5b28d20d6b7404ed7f9
Mike Fix <62121649+mfix-stripe@users.noreply.github.com>
2022-10-24 09:05:29 -0700

    Forward React.forwardRef react nodes (#263)
    * Forward React.forwardRef react nodes
    
    Closes https://github.com/markdoc/markdoc/discussions/255
    
    * duplicate tagName function

--- src/renderers/react/react.ts
+++ src/renderers/react/react.ts
@@ -1,12 +1,27 @@
-import { tagName } from './shared';
 import Tag from '../../tag';
 import { RenderableTreeNodes, Scalar } from '../../types';
-import type { createElement, Fragment, ReactNode } from 'react';
+
+import type { createElement, ComponentType, Fragment, ReactNode } from 'react';
 
 type ReactShape = Readonly<{
   createElement: typeof createElement;
   Fragment: typeof Fragment;
 }>;
+
+type Component = ComponentType<unknown>;
+
+function tagName(
+  name: string,
+  components: Record<string, Component> | ((string: string) => Component)
+): string | Component {
+  return typeof name !== 'string'
+    ? name // This can be an object, e.g. when React.forwardRef is used
+    : name[0] !== name[0].toUpperCase()
+    ? name
+    : components instanceof Function
+    ? components(name)
+    : components[name];
+}
 
 export default function dynamic(
   node: RenderableTreeNodes,
