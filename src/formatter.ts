7bbb0b39e829fd028f1a295d2a33cfd38bc5015e
Bibek Ghimire <56042317+bibek-stripe@users.noreply.github.com>
2024-09-26 17:16:38 -0700

    Add orderedListMode option to Markdoc.format (#523)
    * Incrementing numbers
    
    * prettier
    
    * better name
    
    * change test name
    
    * Change to string union enum

--- src/formatter.ts
+++ src/formatter.ts
@@ -5,6 +5,7 @@ import type { AttributeValue, Function, Node, Value, Variable } from './types';
 type Options = {
   allowIndentation?: boolean;
   maxTagOpeningWidth?: number;
+  orderedListMode?: 'increment' | 'repeat';
   parent?: Node;
   indent?: number;
 };
@@ -302,11 +303,20 @@ function* formatNode(n: Node, o: Options = {}) {
       );
 
       for (let i = 0; i < n.children.length; i++) {
-        const prefix = n.attributes.ordered
-          ? `${i === 0 ? n.attributes.start ?? '1' : '1'}${
-              n.attributes.marker ?? OL
-            }`
-          : n.attributes.marker ?? UL;
+        const prefix = (() => {
+          if (!n.attributes.ordered) return n.attributes.marker ?? UL;
+
+          // Must be an ordered list now
+          let number = '1';
+          const startNumber = n.attributes.start ?? 1;
+          if (i === 0) number = startNumber.toString();
+
+          if (o.orderedListMode === 'increment') {
+            number = (parseInt(startNumber) + i).toString();
+          }
+
+          return `${number}${n.attributes.marker ?? OL}`;
+        })();
         let d = format(n.children[i], increment(no, prefix.length + 1));
 
         if (!isLoose || i === n.children.length - 1) {
