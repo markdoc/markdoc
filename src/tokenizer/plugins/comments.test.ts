2470dcebb165aa59a8ab5a5b55471d8a87cf5fc8
Alistair Gray <ajgray@stripe.com>
2025-04-30 20:03:34 -0400

    format

--- src/tokenizer/plugins/comments.test.ts
+++ src/tokenizer/plugins/comments.test.ts
@@ -104,9 +104,7 @@ describe('MarkdownIt Comments plugin', function () {
         { type: 'paragraph_open' },
         {
           type: 'inline',
-          children: [
-            { type: 'text', content: 'foo <!-- example' },
-          ],
+          children: [{ type: 'text', content: 'foo <!-- example' }],
         },
         { type: 'paragraph_close' },
         { type: 'paragraph_open' },
